01da8381b0bc3650d01190869b983d30a4fe65a5
RoaryStar <hanssun07@gmail.com>
2017-09-16 10:32:08 -0400

    workout generation p1: assigning fatigue levels

--- backend-dev/selection_alg/main.cpp
+++ backend-dev/selection_alg/main.cpp
@@ -2,6 +2,9 @@
 #include <vector>
 #include <string>
 
+#define FOCUS_MULTIPLIER 2.5
+#define FATIGUE_RUST 0.00001
+
 struct gym
 {
     std::string location, name;
@@ -17,12 +20,12 @@ struct gym
 struct exercise
 {
     std::string name, desc;
-    int id, level;
+    int id, level, sec_per_rep;
     std::vector<int> equipment, body_parts;
 
-    exercise(int i, std::string n, std::string d, std::vector<int> e, std::vector<int> b, int l)
+    exercise(int i, std::string n, std::string d, std::vector<int> e, std::vector<int> b, int l, int t)
     {
-        id = i; name = n; desc = d; equipment = e; body_parts = b; level = l;
+        id = i; name = n; desc = d; equipment = e; body_parts = b; level = l; sec_per_rep = t;
     }
 };
 struct preferences
@@ -40,6 +43,7 @@ struct block
 std::vector<gym> gyms;
 std::vector<exercise> exercises;
 int num_types_equipment, num_body_parts;
+preferences p;
 
 int get_data();
 int get_input();
@@ -56,9 +60,27 @@ int main(int argc, const char* argv[])
     return 0;
 }
 
+/* algorithm:
+ * 1. assign each body part an intended fatigue level based on preferences
+ * 2. select blocks of exercises, such that:
+ *    a) every body part worked in an exercise gains fatigue
+ *    b) when a body part isn't being used, it loses fatigue at a much slower rate
+ */
 int generate_workout()
 {
-    printf("generate_workout not yet implemented!\n");
+    //assign intended intensity levels based on user input
+    double intended_fatigue[num_body_parts];
+    const double _it_ftg = num_body_parts * p.intensity; //intended total intensity
+    const double _ut_ftg = (p.focus.size()*FOCUS_MULTIPLIER + (num_body_parts - p.focus.size())) * p.intensity;//unweighted total intensity
+    const double _g_ftg_m = _it_ftg / _ut_ftg;//global intensity multiplier
+    for (int i = 0; i < num_body_parts; ++i)
+        intended_fatigue[i] = p.intensity * _g_ftg_m;
+    for (int i = 0; i < p.focus.size(); ++i)
+        intended_fatigue[p.focus[i]] = FOCUS_MULTIPLIER * p.intensity * _g_ftg_m;
+
+
+
+    printf("generate_workout not yet fully implemented!\n");
     return 0;
 }
 
