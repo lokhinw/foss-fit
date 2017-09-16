12826bd546c41d2d8dbb9d93cd48a526dcda868c
RoaryStar <hanssun07@gmail.com>
2017-09-16 08:59:40 -0400

    gym and exercise structs

--- backend-dev/selection_alg/main.cpp
+++ backend-dev/selection_alg/main.cpp
@@ -1,1 +1,23 @@
+#include <stdio.h>
+#include <vector>
+#include <string>
 
+struct gym
+{
+    std::string location, name;
+    std::vector<int> equipment;
+};
+struct exercise
+{
+    std::string name, desc;
+    int id;
+    std::vector<int> equipment, body_parts;
+};
+
+int main(int argc, const char* argv[])
+{
+
+
+
+    return 0;
+}
